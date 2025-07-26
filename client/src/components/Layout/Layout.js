815c626e65c5db773a68ee176f275c0d003b2cc8
madhav006-skill <amankumar552023@gmail.com>
2025-07-26 22:23:07 +0530

    Re-added client folder after removing .git submodule

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
+const Layout = ({ children, title, description, keywords, author }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description || "mern stack project"} />
+        <meta name="keywords" content={keywords || "mern,react,node,mongodb"} />
+        <meta name="author" content={author || "Techinfoyt"} />
+        <title>{title || "Ecommerce app - shop now"}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>
+        <Toaster />
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Techinfoyt",
+};
+
+export default Layout;
